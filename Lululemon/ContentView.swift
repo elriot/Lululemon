72195ff9c8973bd74826f171244197b1c00fe754
Soopin Kim <spkim8901@gmail.com>
2024-08-26 15:09:08 -0700

    Initial commit

---
+++ Lululemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lululemon
+//
+//  Created by SOOPIN KIM on 2024-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lululemon/LululemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LululemonApp.swift
+//  Lululemon
+//
+//  Created by SOOPIN KIM on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct LululemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
