2c34e69a3fed4ef404400da68c718409df958bec
Soopin Kim <spkim8901@gmail.com>
2024-08-29 10:19:10 -0700

    set classes layout and image set

--- Lululemon/Views/ClassesView/ClassesView.swift
+++ Lululemon/Views/ClassesView/ClassesView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct ClassesView: View {
+    
+    init() {
+        configureNavigationBarAppearance()
+    }
     var body: some View {
-        Text("ClassesView")
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    Image("peleton")
+                        .resizable()
+                        .scaledToFit()
+    //                    .frame(width: .infinity)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Make your workouts world-class.")
+                            .font(SystemFont.AppFont)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding()
+                        
+                        Text("With lululemon Membership, you get access to free Peleton classes-no trial necessary. This month's theme is sleep health. Incorportate te following classes into your routine to calm the min and relax the body.")
+                            .font(SystemFont.semiBasicFont)
+                            .padding()
+                            .lineLimit(nil)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Looking for more?")
+                            .font(SystemFont.AppFont)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding()
+                        
+                        Text("Try the Peleton App for free and unlock thousnads of classes with lululemon Membership's newest benefit: Parter perks.")
+                            .font(SystemFont.semiBasicFont)
+                            .padding()
+                    }
+
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Looking for more?")
+                            .font(SystemFont.AppFont)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding()
+                        
+                        Text("Try the Peleton App for free and unlock thousnads of classes with lululemon Membership's newest benefit: Parter perks.")
+                            .font(SystemFont.semiBasicFont)
+                            .padding()
+                    }
+                }
+            }
+
+            .navigationTitle("Select Peleton Classes")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
