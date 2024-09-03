efc1635ddce933916d2e66c8474e25e75d5cf604
Soopin Kim <spkim8901@gmail.com>
2024-09-03 14:48:17 -0700

    AccountView Top Done

--- Lululemon/Views/AccountView/AccountView.swift
+++ Lululemon/Views/AccountView/AccountView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        Text("AccountView")
+        VStack(alignment: .center) {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Discover lululemon")
+                    Spacer()
+                }
+                HStack {
+                    Text("Membership")
+                    Spacer()
+                }
+            }
+            .font(SystemFont.AppFont)
+            .fontWeight(.bold)
+            .padding(EdgeInsets(top: 50, leading: 20, bottom: 50, trailing: 20))
+            
+            
+            VStack (spacing: 20){
+                HStack {
+                    Text("One account. Tons of benefits. Endless possibilites.")
+                    Spacer()
+                }
+                
+                HStack {
+                    Text("Experience membership - It's free")
+                    Image(systemName: "chevron.right")
+                    Spacer()
+                }
+                .fontWeight(.semibold)
+                
+            }
+            .font(SystemFont.BasicFont)
+            .foregroundColor(.darkgray)
+            .padding(EdgeInsets(top: 10, leading: 20, bottom: 30, trailing: 20))
+            
+
+        }
+        
+        .background(.lightGray.opacity(0.5))
+        
     }
 }
 
