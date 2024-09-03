b9749d9ead20bdd5c8a8b43f4b6908f058a5fe97
Soopin Kim <spkim8901@gmail.com>
2024-09-03 14:01:49 -0700

    Basic Structor of WishList

--- Lululemon/Views/WishListView/WishlistView.swift
+++ Lululemon/Views/WishListView/WishlistView.swift
@@ -8,11 +8,78 @@
 import SwiftUI
 
 struct WishlistView: View {
+    
+    init() {
+        configureNavigationBarAppearance()
+    }
+    
     var body: some View {
-        Text("WishlistView")
+        NavigationView {
+            VStack {
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Text("Already have a wishlist?")
+                                .fontWeight(.bold)
+                            
+                            
+                            Spacer()
+                            
+                            Text("Sign In")
+                            Image(systemName: "chevron.right")
+                                .fontWeight(.semibold)
+                        }
+                        
+                    }
+                    .font(SystemFont.BasicFont)
+                    .buttonStyle(DefaultButtonStyle())
+                }
+                .padding()
+                .overlay(
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.lightGray),
+                    alignment: .bottom
+                )
+                
+                VStack {
+                    HStack(alignment: .center) {
+                        Text("Your wish is")
+                        Spacer()
+                    }
+                    
+                    HStack(alignment: .center) {
+                        Text("our command.")
+                        Spacer()
+                    }
+                    
+                }
+                .font(SystemFont.AppFont)
+                .fontWeight(.bold)
+                .padding(EdgeInsets(top: 50, leading: 20, bottom: 50, trailing: 20))
+                
+                
+                Spacer()
+            }
+            .foregroundColor(.black)
+            .navigationTitle("Wish List")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        print("bag")
+                    }) {
+                        Image(systemName: "bag")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
+        
     }
 }
-
 #Preview {
     WishlistView()
 }
